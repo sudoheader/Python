d927b2f71758332974d5b8f1af91ac9c76b2b992
sudoheader <sudoheader@users.noreply.github.com>
2017-08-20 22:45:21 -0700

    Object Oriented programming in Python

--- oop.py
+++ oop.py
@@ -340,3 +340,16 @@ class Pizza:
 ingredients = ["cheese", "onions", "spam"]
 if all(Pizza.validate_topping(i) for i in ingredients):
     pizza = Pizza(ingredients)
+
+# Properties
+class Pizza:
+  def __init__(self, toppings):
+    self.toppings = toppings
+
+  @property
+  def pineapple_allowed(self):
+    return False
+
+pizza = Pizza(["cheese", "tomato"])
+print(pizza.pineapple_allowed)
+pizza.pineapple_allowed = True
