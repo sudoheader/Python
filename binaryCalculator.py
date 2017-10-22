db207439af6fda685939571b21544260eff9f5c8
Raymond Nazaryan <rayn2427@gmail.com>
2017-10-21 22:40:15 -0700

    Added binaryCalculator.py (only does addition)

---
+++ binaryCalculator.py
@@ -0,0 +1,13 @@
+x = int(input("Enter your number: "))
+if x < 256:
+    print(bin(x))
+else:
+    print("Your value is too high ")
+
+y = int(input("Enter your number: "))
+y = int(y)
+if y < 256:
+    print(bin(x))
+else:
+    print("Your value is too high")
+print("Your answer is ", x + y, " the binary is ", bin(x + y))
